ec0b6b44107805930650dcd75715c6b7717cc899
pe-pa-mario <336699ren@gmail.com>
2022-07-06 05:32:48 +0000

    first commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,20 @@
 class BooksController < ApplicationController
+  before_action :correct_user, only: [:edit,:update]
+
   def new
   end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
+   if @book.save
+    flash[:notice] = "You have created book successfully."
+    redirect_to book_path(@book.id)
+   else
+    @user = current_user
+    @books = Book.all
+    render :index
+   end
   end
 
   def index
@@ -18,18 +26,31 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     @books = Book.all
-    @user = current_user
+    @user = @book.user
+    @book_new = Book.new
 
   end
 
   def edit
-  @book = Book.find(params[:id])
+    @book = Book.find(params[:id])
+
+  end
+
+  def update
+    @book = Book.find(params[:id])
+   if @book.update (book_params)
+    flash[:notice] = "You have updated books successfully"
+    redirect_to book_path (@book.id)
+   else
+     render :edit
+   end
+
   end
 
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
-    redirect_to '/book'
+    redirect_to books_path
   end
 
    private
@@ -37,4 +58,11 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body, :name)
   end
+
+  def correct_user
+    @book = Book.find(params[:id])
+    @user = @book.user
+    redirect_to(books_path) unless @user == current_user
+  end
+
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,12 +1,7 @@
 class Book < ApplicationRecord
    belongs_to :user
 
-    def get_image
-    if image.attached?
-      image
-    else
-      'no_image.jpg'
-    end
-　 end
 
+validates :title, presence: true
+validates :body, presence: true, length: {maximum: 200}
 end
